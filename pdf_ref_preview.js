38f3ebf4e487e5e7501260b57168e44c0b224bb5
Ben Ling <belinghy@gmail.com>
2020-05-26 22:19:21 -0700

    Fix anchor to page so preview will render on top of everything

--- pdf_ref_preview.js
+++ pdf_ref_preview.js
@@ -1,22 +1,29 @@
 (async () => {
   const app = window.PDFViewerApplication;
-  if ('_previewHandler' in app) {
-    document.removeEventListener('mouseover', app._previewHandler);
+  if ("_previewHandler" in app) {
+    document.removeEventListener("mouseover", app._previewHandler);
     delete app._previewHandler;
+    delete app._previewing;
     return;
   }
 
   const destinations = await app.pdfDocument.getDestinations();
+  const anchor = document.getElementById("viewer");
+  app._previewing = false;
 
   async function mouseoverHandler(event) {
-    if (event.target.className != 'internalLink') return;
+    if (event.target.className != "internalLink" || app._previewing) return;
 
     const hash = event.target.hash;
     const parent = event.target.parentElement;
 
-    const preview = document.createElement('canvas');
-    preview.style.border = '1px solid black';
-    preview.style.direction = 'ltr';
+    const preview = document.createElement("canvas");
+    const previewStyle = preview.style;
+    previewStyle.border = "1px solid black";
+    previewStyle.direction = "ltr";
+    previewStyle.position = "fixed";
+    previewStyle.zIndex = "2";
+    previewStyle.top = `${event.clientY + 4}px`;
 
     const namedDest = decodeURIComponent(hash.substring(1));
     const explicitDest =
@@ -27,32 +34,53 @@
 
     app.pdfDocument.getPage(pageNumber).then(function (page) {
       const tempViewport = page.getViewport({ scale: 1.0 });
-      preview.style.height = `${tempViewport.height}px`;
-      preview.style.width = `${tempViewport.width}px`;
+      const height = tempViewport.height * 1.2 * app.pdfViewer.currentScale;
+      const width = tempViewport.width * 1.2 * app.pdfViewer.currentScale;
+      previewStyle.height = `${height}px`;
+      previewStyle.width = `${width}px`;
+      previewStyle.left = `${event.clientX - width / 3 - 4}px`;
 
-      const scale = 4.0;
-      const viewport = page.getViewport({
-        scale: scale,
-        offsetX: -explicitDest[2] * scale,
-        offsetY: (explicitDest[3] - tempViewport.height) * scale,
-      });
+      let viewport;
+      const scale = 4;
+      switch (explicitDest[1].name) {
+        case "XYZ":
+          viewport = page.getViewport({
+            scale: scale,
+            offsetX: -explicitDest[2] * scale,
+            offsetY: (explicitDest[3] - tempViewport.height) * scale,
+          });
+          break;
+        case "FitH":
+        case "FitBH":
+        case "FitV":
+        case "FitBV":
+          viewport = page.getViewport({
+            scale: scale,
+            offsetY: (explicitDest[2] - tempViewport.height) * scale,
+          });
+          break;
+        default:
+          console.log(`Oops, link ${explicitDest[1].name} is not supported.`);
+      }
 
       preview.height = viewport.height;
       preview.width = viewport.width;
 
       const renderContext = {
-        canvasContext: preview.getContext('2d'),
+        canvasContext: preview.getContext("2d"),
         viewport: viewport,
       };
       page.render(renderContext);
     });
 
-    parent.appendChild(preview);
+    anchor.prepend(preview);
+    app._previewing = true;
 
-    parent.addEventListener('mouseleave', function (event) {
+    parent.addEventListener("mouseleave", function (event) {
       preview.remove();
+      app._previewing = false;
     });
   }
-  document.addEventListener('mouseover', mouseoverHandler);
+  document.addEventListener("mouseover", mouseoverHandler);
   app._previewHandler = mouseoverHandler;
 })();
